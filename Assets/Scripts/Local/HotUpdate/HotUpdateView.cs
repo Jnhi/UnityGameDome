19d5fdb9cdb5b6ce140a116e6010546662e7a3b7
周建辉 <603061787@qq.com>
2022-12-02 00:07:38 +0800

    热更新测试

--- Assets/Scripts/Local/HotUpdate/HotUpdateView.cs
+++ Assets/Scripts/Local/HotUpdate/HotUpdateView.cs
@@ -1,34 +1,40 @@
 using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
-using HFramework;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
-using UnityEngine.ResourceManagement.ResourceLocations;
 using System.IO;
+using System.Collections;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using static UnityEngine.AddressableAssets.Addressables;
 
 public class HotUpdateView : MonoBehaviour {
     private Button btnSure;
     private Button btnCanel;
     private TMP_Text txtInfo;
-    private TMP_Text txtLoading;
     private Slider prgLoading;
+
     void Awake() {
         btnSure = this.transform.Find("btnSure").GetComponent<Button>();
         btnCanel = this.transform.Find("btnCanel").GetComponent<Button>();
         txtInfo = this.transform.Find("txtInfo").GetComponent<TMP_Text>();
-        txtLoading = this.transform.Find("txtLoading").GetComponent<TMP_Text>();
         prgLoading = this.transform.Find("prgLoading").GetComponent<Slider>();
         prgLoading.value = 0;
         btnSure.gameObject.SetActive(false);
         btnCanel.gameObject.SetActive(false);
     }
-    async void Start(){
-        await CheckUpdate();
+    void Start()
+    {
+        StartCoroutine(DoUpdateAddressadble());
     }
 
+    // async Task Start()
+    // {
+    //     await CheckUpdate();
+    // }
     public async Task CheckUpdate()
     {
         var start = DateTime.Now;
@@ -45,14 +51,15 @@ public class HotUpdateView : MonoBehaviour {
             txtInfo.text = "正在更新资源...";
             start = DateTime.Now;
             Debug.Log("2222");
-            var locators = await Addressables.UpdateCatalogs(catalogs, false).Task;
-            Debug.Log("333");
-            foreach (var locator in locators)
+            List<IResourceLocator> locators = await Addressables.UpdateCatalogs(catalogs, false).Task;
+            foreach (var v in locators)
             {
-                Debug.Log(locator);
-                var size = await Addressables.GetDownloadSizeAsync(locator.Keys).Task;
-                Debug.Log($"download size:{size}");
-                txtInfo.text = $"本次更新大小：{size}";
+                List<object> keys = new List<object>();
+                keys.AddRange(v.Keys);
+                Debug.Log(v);
+                var size = await Addressables.GetDownloadSizeAsync(keys).Task;
+                Debug.Log($"download size:{size/Math.Pow(1024,2)}");
+                txtInfo.text = $"本次更新大小：{size}MB";
                 // btnSure.gameObject.SetActive(true);
                 // btnCanel.gameObject.SetActive(true);
                 // btnSure.onClick.AddListener(()=>{
@@ -60,21 +67,21 @@ public class HotUpdateView : MonoBehaviour {
                 //     btnCanel.gameObject.SetActive(false);
 
                 // });
-                var downloadHandle = Addressables.DownloadDependenciesAsync(locator.Keys, Addressables.MergeMode.Union);
+                var downloadHandle = Addressables.DownloadDependenciesAsync(keys,MergeMode.Union,false);
                 while (!downloadHandle.IsDone)
                 {
                     float percentage = downloadHandle.PercentComplete;
                     Debug.Log($"download pregress: {percentage}");
-                    prgLoading.value = percentage * 100;
+                    // prgLoading.value = percentage * 100;
 
                 }
                 Addressables.Release(downloadHandle);
             }
             UpdateFinish();
             
-            // Addressables.Release(locators);
+            Addressables.Release(locators);
 
-            // Addressables.Release(catalogs);
+            Addressables.Release(catalogs);
         }
         
 
@@ -100,12 +107,102 @@ public class HotUpdateView : MonoBehaviour {
         Debug.Log("AssetManager init done");      
     }
 
+    IEnumerator DoUpdateAddressadble()
+    {
+        string _catalogPath = Application.persistentDataPath +"/com.unity.addressables";
+        if(Directory.Exists(_catalogPath))
+        {
+            try
+            {
+                Directory.Delete(_catalogPath,true);
+                Debug.Log("delete catalog cache done!");
+            }
+            catch(Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
+        }
+
+        AsyncOperationHandle<IResourceLocator> initHandle = Addressables.InitializeAsync();
+        yield return initHandle;
+
+        // 检测更新
+        var checkHandle = Addressables.CheckForCatalogUpdates(false);
+        yield return checkHandle;
+        if (checkHandle.Status != AsyncOperationStatus.Succeeded)
+        {
+            Debug.Log("CheckForCatalogUpdates Error\n" +  checkHandle.OperationException.ToString());
+            yield break;
+        }
+
+        if (checkHandle.Result.Count > 0)
+        {
+            var updateHandle = Addressables.UpdateCatalogs(checkHandle.Result, false);
+            yield return updateHandle;
+
+            if (updateHandle.Status != AsyncOperationStatus.Succeeded)
+            {
+                Debug.Log("UpdateCatalogs Error\n" + updateHandle.OperationException.ToString());
+                yield break;
+            }
+
+            // 更新列表迭代器
+            List<IResourceLocator> locators = updateHandle.Result;
+            foreach (var locator in locators)
+            {
+                List<object> keys = new List<object>();
+                keys.AddRange(locator.Keys);
+                // 获取待下载的文件总大小
+                var sizeHandle = Addressables.GetDownloadSizeAsync(keys);
+                yield return sizeHandle;
+                if (sizeHandle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    Debug.Log("GetDownloadSizeAsync Error\n" + sizeHandle.OperationException.ToString());
+                    yield break;
+                }
+
+                long totalDownloadSize = sizeHandle.Result;
+                Debug.Log($"download size:{totalDownloadSize/Math.Pow(1024,2)}");
+                if (totalDownloadSize > 0)
+                {
+                    // 下载
+                    var downloadHandle = Addressables.DownloadDependenciesAsync(keys,MergeMode.Union,false);
+                    while (!downloadHandle.IsDone)
+                    {
+                        if (downloadHandle.Status == AsyncOperationStatus.Failed)
+                        {
+                            Debug.Log("DownloadDependenciesAsync Error\n"  + downloadHandle.OperationException.ToString());
+                            yield break;
+                        }
+                        // 下载进度
+                        float percentage = downloadHandle.PercentComplete;
+                        prgLoading.value = percentage * 100;
+                        Debug.Log($"已下载: {percentage}");
+                        txtInfo.text = $"\n已下载: {percentage}";
+                        yield return null;
+                    }
+                    if (downloadHandle.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        Debug.Log("下载完毕!");
+                        txtInfo.text = "下载完毕";
+                        UpdateFinish();
+                    }
+                }
+            }
+        }
+        else
+        {
+            txtInfo.text = txtInfo.text + "\n没有检测到更新";
+        }
+
+        // 进入游戏
+        StartGame();
+    }
+
     void StartGame()
     {
         txtInfo.text = "正在进入游戏...";
         Debug.Log("进入游戏");
-        // JsManager.Instance.StartGame();
-
     }
 
     void UpdateFinish()
